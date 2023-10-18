3b5e6e6ec82099b35bbce1681ae40ea937a0d93a
Auriel3003 <116882665+Auriel3003@users.noreply.github.com>
2023-10-18 20:04:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios'], // Specify Axios as an external dependency
-    },
-  },
-});
+  plugins: [react()]
+})
